0876cc829e36d4c85d28a1aa92e7249fa9b3aef9
Mauricio <mauriciogoulart.1990@gmail.com>
2025-03-04 19:29:58 -0300

    feat: importMongoose on category module

--- src/categories/categories.module.ts
+++ src/categories/categories.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { CategoriesController } from './categories.controller';
 import { CategoriesService } from './categories.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { CategorySchema } from './interfaces/categories.schema';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([{ name: 'Category', schema: CategorySchema }]),
+  ],
   controllers: [CategoriesController],
-  providers: [CategoriesService]
+  providers: [CategoriesService],
 })
 export class CategoriesModule {}
