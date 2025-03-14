dde8a5caca885f362fd6ce83d35ce9be99a4e51c
Mauricio <mauriciogoulart.1990@gmail.com>
2025-03-13 21:33:39 -0300

    update challenges.service.ts

--- src/challenges/challenges.service.ts
+++ src/challenges/challenges.service.ts
@@ -1,4 +1,13 @@
 import { Injectable } from "@nestjs/common";
+import { CategoriesService } from "src/categories/categories.service";
+import { Challenge } from "./interfaces/challenges.interfaces";
+import { CreateChallengeDto } from "./dtos/create-challenge.dto";
 
 @Injectable()
-export class ChallengesService {}
+export class ChallengesService {
+  constructor(private readonly categoriesService: CategoriesService) {}
+
+  async createChallenge(
+    createChallengeDto: CreateChallengeDto
+  ): Promise<Challenge> {}
+}
