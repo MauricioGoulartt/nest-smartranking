c728d16d63e53cc22fec1d15ff21b15f787266f5
Mauricio <mauriciogoulart.1990@gmail.com>
2025-03-13 21:23:34 -0300

    update challenges.controller.ts

--- src/challenges/challenges.controller.ts
+++ src/challenges/challenges.controller.ts
@@ -1,4 +1,49 @@
-import { Controller } from "@nestjs/common";
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Logger,
+  Post,
+  Put,
+  UsePipes,
+  ValidationPipe,
+} from "@nestjs/common";
+import { ChallengesService } from "./challenges.service";
+import { CreateChallengeDto } from "./dtos/create-challenge.dto";
+import { Challenge } from "./interfaces/challenges.interfaces";
 
-@Controller("challenges")
-export class ChallengesController {}
+@Controller("api/v1/challenges")
+export class ChallengesController {
+  constructor(private readonly challengesService: ChallengesService) {}
+
+  private readonly logger = new Logger(ChallengesController.name);
+
+  @Post()
+  @UsePipes(ValidationPipe)
+  async createChallenge(
+    @Body() createChallengeDto: CreateChallengeDto
+  ): Promise<Challenge> {
+    return await this.challengesService.createChallenge(createChallengeDto);
+  }
+
+  @Get()
+  async getChallenges(): Promise<Challenge[]> {
+    return await this.challengesService.getChallenges();
+  }
+
+  @Put("/:challenge")
+  async updateChallenge(): Promise<void> {
+    await this.challengesService.updateChallenge();
+  }
+
+  @Post()
+  async attachChallangeMatch(): Promise<void> {
+    await this.challengesService.attachChallenge();
+  }
+
+  @Delete("/:challenge")
+  async deleteChallenges(): Promise<void> {
+    await this.challengesService.deleteChallenge();
+  }
+}
